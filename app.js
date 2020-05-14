b1299d84e9ff7b9376c65f7886865fe212d8b9cf
Yankee1234 <vovandemort2013@gmail.com>
2020-05-14 10:44:14 +0300

    app.js updated

--- app.js
+++ app.js
@@ -2,4 +2,17 @@ const express = require('express');
 const app = express();
 const config = require('./config/config');
 
+const hbs = require('hbs');
+const TodoRoute = require('./routes/TodoRoute')
+app.set('view engine', 'hbs');
 
+hbs.registerPartials(__dirname + '/views/partials');
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(__dirname + '/public'));
+
+app.use('/TDList', TodoRoute);
+
+app.listen(config.port, config.host, () => {
+    console.log('Server has started');
+})
